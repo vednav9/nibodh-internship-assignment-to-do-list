a32d30cc64e8871662690261a7c297616268ee67
Vedant Navthale <vedantnavthale@gmail.com>
2022-08-08 15:20:09 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,127 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+
+  componentDidMount() {
+    this.hydrateStateWithLocalStorage();
+
+    window.addEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+
+    this.saveStateToLocalStorage();
+  }
+
+  hydrateStateWithLocalStorage() {
+
+    for (let key in this.state) {
+
+      if (localStorage.hasOwnProperty(key)) {
+        let value = localStorage.getItem(key);
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  }
+
+  saveStateToLocalStorage() {
+    for (let key in this.state) {
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
+
+  updateInput(key, value) {
+    this.setState({ [key]: value });
+  }
+
+  addItem() {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+ 
+    };
+
+    const list = [...this.state.list];
+    list.push(newItem);
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({ list: updatedList });
+  }
+  
+  render() {
+    return (
+      <div>
+
+      <h1>MY LIST</h1>
+        
+        <div>
+        <div
+          style={{
+            padding: 30,
+            textAlign: "left",
+            maxWidth: 500,
+            margin: "auto"
+          }}
+        >
+          Add an Item...
+          <br />
+          <input
+            type="text"
+            placeholder="Type item here"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <button
+            onClick={() => this.addItem()}
+            disabled={!this.state.newItem.length}
+          >
+            <i> + </i>
+          </button>
+          <br /> <br />
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button onClick={() => this.deleteItem(item.id)}>
+                    <i>x</i>
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
